5f9281e86748a12826e3a566f6cd98f2b1712f6d
Patrick Zimmerer <patrick.zimmerer1@gmail.com>
2022-03-30 17:28:23 +0200

    added contact me section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { MyWorkComponent } from './my-work/my-work.component';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,12 +23,16 @@ import { AboutMeComponent } from './about-me/about-me.component';
     SkillsComponent,
     MyWorkComponent,
     AboutMeComponent,
+    ContactMeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
     BrowserAnimationsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
